61851582a959a8521b8a773bed6a1f2fae966359
Andrei Catinean <andrei@novoda.com>
2017-11-07 11:19:05 +0000

    Init commit

---
+++ feature/src/main/java/com/novoda/feature/SimpleForegroundService.java
@@ -0,0 +1,41 @@
+package com.novoda.feature;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+
+public class SimpleForegroundService extends Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        Notification notification =
+                new NotificationCompat.Builder(this, "default")
+                        .setContentTitle("Simple Service")
+                        .setContentText("This is just a test")
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentIntent(pendingIntent)
+                        .setTicker("This is just a test")
+                        .build();
+        startForeground(1, notification);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+}
